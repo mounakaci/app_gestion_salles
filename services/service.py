64bc6232a22d7a20bdafea5dd8b39b4ea857b584
mouna <300156497@monboreal.ca>
2026-04-19 19:27:48 -0400

    commition

--- services/service.py
+++ services/service.py
@@ -8,8 +8,27 @@ class ServiceSalle:
         if salle.code and salle.description and salle.categorie and salle.capacite:
             if salle.capacite >= 1:
                 self.dao_salle.insert_salle(salle)
-                return True, "salle ajoutée avec succès"
+                return True
             else:
-                return False, "capacité invalide"
+                return False
         else:
-            return False, "données invalides"
+            return False
+
+    def supprimer_salle(self, code):
+            self.dao_salle.delete_salle(code)
+
+    def rechercher_salle(self, code):
+            return self.dao_salle.get_salle(code)
+
+    def recuperer_salles(self):
+            return self.dao_salle.get_salles()
+
+    def modifier_salle(self, salle):
+        if salle.code and salle.description and salle.categorie and salle.capacite:
+            if salle.capacite >= 1:
+                self.dao_salle.update_salle(salle)
+                return True
+            else:
+                return False
+        else:
+            return False
