bf96e9812cce6edfee66010ec96d2f80bc82b95c
mouna <300156497@monboreal.ca>
2026-04-19 19:21:29 -0400

    commition

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -27,4 +27,51 @@ class DataSalle:
         crs.close()
         con.close()
 
-   
+    def update_salle(self, salle):
+        con = self.get_connection()
+        crs = con.cursor()
+        crs.execute(
+            "UPDATE salle SET description=%s, categorie=%s, capacite=%s WHERE code=%s",
+            (salle.description, salle.categorie, salle.capacite, salle.code)
+        )
+        con.commit()
+        crs.close()
+        con.close()
+
+    def delete_salle(self, code):
+        con = self.get_connection()
+        crs = con.cursor()
+        crs.execute(
+            "DELETE FROM salle WHERE code=%s",
+            (code,)
+        )
+        con.commit()
+        crs.close()
+        con.close()
+
+    def get_salle(self, code):
+        con = self.get_connection()
+        crs = con.cursor()
+        crs.execute(
+            "SELECT * FROM salle WHERE code=%s",
+            (code,)
+        )
+        row = crs.fetchone()
+        crs.close()
+        con.close()
+
+        return Salle(row[0], row[1], row[2], row[3])
+
+    def get_salles(self):
+        con = self.get_connection()
+        crs = con.cursor()
+        crs.execute("SELECT * FROM salle")
+        rows = crs.fetchall()
+        crs.close()
+        con.close()
+
+        salles = []
+        for row in rows:
+            salles.append(Salle(row[0], row[1], row[2], row[3]))
+
+        return salles
