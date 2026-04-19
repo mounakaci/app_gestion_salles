03ad2cdf28ec2d21a759fa0b9a0d43c7c6cbd5f9
mouna <300156497@monboreal.ca>
2026-04-19 19:34:14 -0400

    commition

--- views/view_salle.py
+++ views/view_salle.py
@@ -9,6 +9,15 @@ class ViewSalle(ctk.CTk):
         super().__init__()
 
         self.service_salle = ServiceSalle()
-
         self.title("Gestion Salles")
         self.geometry("800x500")
+        self.frame_infos = ctk.CTkFrame(self)
+        self.frame_infos.pack()
+        self.code = ctk.CTkEntry(self.frame_infos)
+        self.code.pack()
+        self.description = ctk.CTkEntry(self.frame_infos)
+        self.description.pack()
+        self.categorie = ctk.CTkEntry(self.frame_infos)
+        self.categorie.pack()
+        self.capacite = ctk.CTkEntry(self.frame_infos)
+        self.capacite.pack()
